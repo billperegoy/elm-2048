168f93304ce6db9a532ac5ab5e6c97e80d5d6781
Bill Peregoy <bill@gnar.dog>
2018-09-03 17:22:24 -0400

    Initial commit

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,13 @@
 module Main exposing (..)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Utils
 
 
-main : Program Never Model Msg
+main : Program Flags Model Msg
 main =
-    Html.program
+    Browser.element
         { init = init
         , view = view
         , update = update
@@ -15,17 +15,43 @@ main =
         }
 
 
+type alias Flags =
+    ()
+
+
 
 -- Model
 
 
-type alias Model =
-    { name : String }
+type Element
+    = Empty
+    | Full Int
 
 
-init : ( Model, Cmd Msg )
-init =
-    Model "world" ! []
+type Line
+    = Line Element Element Element Element
+
+
+type Model
+    = Board Line Line Line Line
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    let
+        row_1 =
+            Line Empty Empty Empty Empty
+
+        row_2 =
+            Line (Full 2) Empty Empty Empty
+
+        row_3 =
+            Line Empty (Full 2) (Full 2) Empty
+
+        row_4 =
+            Line (Full 4) (Full 4) (Full 2) (Full 2)
+    in
+        ( Board row_1 row_2 row_3 row_4, Cmd.none )
 
 
 
@@ -40,7 +66,7 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model ! []
+            ( model, Cmd.none )
 
 
 
@@ -49,8 +75,118 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    h1 []
-        [ text ("Hello " ++ (Utils.capitalize model.name)) ]
+    h1 [] [ text "Hello" ]
+
+
+lineToList : Line -> List Element
+lineToList (Line elem_1 elem_2 elem_3 elem_4) =
+    [ elem_1, elem_2, elem_3, elem_4 ]
+
+
+listToLine : List Element -> Line
+listToLine elems =
+    case elems of
+        [ elem_1, elem_2, elem_3, elem_4 ] ->
+            Line elem_1 elem_2 elem_3 elem_4
+
+        _ ->
+            Debug.todo "Received list with other than 4 elements"
+
+
+boardToList : Model -> List Line
+boardToList (Board line_1 line_2 line_3 line_4) =
+    [ line_1, line_2, line_3, line_4 ]
+
+
+transformLine : Line -> Line
+transformLine line =
+    shiftLineRight line
+
+
+shiftLineRight : Line -> Line
+shiftLineRight element =
+    case element of
+        -- One active element
+        Line elem Empty Empty Empty ->
+            Line Empty Empty Empty elem
+
+        Line Empty elem Empty Empty ->
+            Line Empty Empty Empty elem
+
+        Line Empty Empty elem Empty ->
+            Line Empty Empty Empty elem
+
+        Line Empty Empty Empty elem ->
+            Line Empty Empty Empty elem
+
+        -- Two active elements
+        Line elem1 elem2 Empty Empty ->
+            Line Empty Empty elem1 elem2
+
+        Line elem1 Empty elem2 Empty ->
+            Line Empty Empty elem1 elem2
+
+        Line elem1 Empty Empty elem2 ->
+            Line Empty Empty elem1 elem2
+
+        Line Empty elem1 elem2 Empty ->
+            Line Empty Empty elem1 elem2
+
+        Line Empty elem1 Empty elem2 ->
+            Line Empty Empty elem1 elem2
+
+        Line Empty Empty elem1 elem2 ->
+            Line Empty Empty elem1 elem2
+
+        -- Three active elements
+        Line elem1 elem2 elem3 Empty ->
+            Line Empty elem1 elem2 elem3
+
+        Line elem1 elem2 Empty elem3 ->
+            Line Empty elem1 elem2 elem3
+
+        Line elem1 Empty elem2 elem3 ->
+            Line Empty elem1 elem2 elem3
+
+        Line Empty elem1 elem2 elem3 ->
+            Line Empty elem1 elem2 elem3
+
+        -- Four active elements
+        Line elem1 elem2 elem3 elem4 ->
+            Line elem1 elem2 elem3 elem4
+
+
+combineLineRight : Line -> Line
+combineLineRight element =
+    case element of
+        Line Empty Empty (Full c) (Full d) ->
+            if c == d then
+                Line Empty Empty Empty (Full (c + d))
+            else
+                element
+
+        Line Empty (Full b) (Full c) (Full d) ->
+            if (b == c) && (c /= d) then
+                Line Empty Empty (Full (b + c)) (Full d)
+            else if (b /= c) && (c == d) then
+                Line Empty Empty (Full b) (Full (c + d))
+            else
+                element
+
+        Line (Full a) (Full b) (Full c) (Full d) ->
+            if a == b && c == d then
+                Line Empty Empty (Full (a + b)) (Full (c + d))
+            else if a /= b && b == c && c /= d then
+                Line Empty (Full a) (Full (b + c)) (Full d)
+            else if a == b && b /= c then
+                Line Empty (Full (a + b)) (Full c) (Full d)
+            else if b /= c && c == d then
+                Line Empty (Full a) (Full b) (Full (c + d))
+            else
+                element
+
+        _ ->
+            element
 
 
 
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,16 +3,240 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
-import Utils
+import Main exposing (..)
 
 
 all : Test
 all =
     describe "A Test Suite"
-        [ test "Capitalizes a lower case word" <|
+        [ test "shiftLineRight, 0 elements" <|
             \() ->
-                Expect.equal (Utils.capitalize "john") "John"
-        , test "Does not change an already capitalized word" <|
+                let
+                    input =
+                        Line Empty Empty Empty Empty
+
+                    expected =
+                        Line Empty Empty Empty Empty
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 1 element, pos 1" <|
             \() ->
-                Expect.equal (Utils.capitalize "John") "John"
+                let
+                    input =
+                        Line (Full 1) Empty Empty Empty
+
+                    expected =
+                        Line Empty Empty Empty (Full 1)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 1 element, pos 2" <|
+            \() ->
+                let
+                    input =
+                        Line Empty (Full 1) Empty Empty
+
+                    expected =
+                        Line Empty Empty Empty (Full 1)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 1 element, pos 3" <|
+            \() ->
+                let
+                    input =
+                        Line Empty Empty (Full 1) Empty
+
+                    expected =
+                        Line Empty Empty Empty (Full 1)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 1 element, pos 4" <|
+            \() ->
+                let
+                    input =
+                        Line Empty Empty Empty (Full 1)
+
+                    expected =
+                        Line Empty Empty Empty (Full 1)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 2 elements, pos 1,2" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 2) Empty Empty
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 2)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 2 elements, pos 1,3" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) Empty (Full 2) Empty
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 2)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 2 elements, pos 1,4" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) Empty Empty (Full 2)
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 2)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 2 elements, pos 2,3" <|
+            \() ->
+                let
+                    input =
+                        Line Empty (Full 1) (Full 2) Empty
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 2)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 2 elements, pos 2,4" <|
+            \() ->
+                let
+                    input =
+                        Line Empty (Full 1) Empty (Full 2)
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 2)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 2 elements, pos 3,4" <|
+            \() ->
+                let
+                    input =
+                        Line Empty Empty (Full 1) (Full 2)
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 2)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 3 elements, pos 1,2,3" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 2) (Full 4) Empty
+
+                    expected =
+                        Line Empty (Full 1) (Full 2) (Full 4)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 3 elements, pos 1,3,4" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) Empty (Full 2) (Full 4)
+
+                    expected =
+                        Line Empty (Full 1) (Full 2) (Full 4)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 3 elements, pos 1,2,4" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 2) Empty (Full 4)
+
+                    expected =
+                        Line Empty (Full 1) (Full 2) (Full 4)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 3 elements, pos 2,3,4" <|
+            \() ->
+                let
+                    input =
+                        Line Empty (Full 1) (Full 2) (Full 4)
+
+                    expected =
+                        Line Empty (Full 1) (Full 2) (Full 4)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "shiftLineRight, 4 elements" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 2) (Full 4) (Full 8)
+
+                    expected =
+                        Line (Full 1) (Full 2) (Full 4) (Full 8)
+                in
+                    Expect.equal (shiftLineRight input) expected
+        , test "combineLineRight 1" <|
+            \() ->
+                let
+                    input =
+                        Line Empty Empty (Full 1) (Full 1)
+
+                    expected =
+                        Line Empty Empty Empty (Full 2)
+                in
+                    Expect.equal (combineLineRight input) expected
+        , test "combineLineRight 2" <|
+            \() ->
+                let
+                    input =
+                        Line Empty (Full 1) (Full 1) (Full 4)
+
+                    expected =
+                        Line Empty Empty (Full 2) (Full 4)
+                in
+                    Expect.equal (combineLineRight input) expected
+        , test "combineLineRight 3" <|
+            \() ->
+                let
+                    input =
+                        Line Empty (Full 1) (Full 2) (Full 2)
+
+                    expected =
+                        Line Empty Empty (Full 1) (Full 4)
+                in
+                    Expect.equal (combineLineRight input) expected
+        , test "combineLineRight 4" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 1) (Full 4) (Full 4)
+
+                    expected =
+                        Line Empty Empty (Full 2) (Full 8)
+                in
+                    Expect.equal (combineLineRight input) expected
+        , test "combineLineRight 5" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 2) (Full 2) (Full 8)
+
+                    expected =
+                        Line Empty (Full 1) (Full 4) (Full 8)
+                in
+                    Expect.equal (combineLineRight input) expected
+        , test "combineLineRight 6" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 1) (Full 4) (Full 8)
+
+                    expected =
+                        Line Empty (Full 2) (Full 4) (Full 8)
+                in
+                    Expect.equal (combineLineRight input) expected
+        , test "combineLineRight 7" <|
+            \() ->
+                let
+                    input =
+                        Line (Full 1) (Full 2) (Full 4) (Full 4)
+
+                    expected =
+                        Line Empty (Full 1) (Full 2) (Full 8)
+                in
+                    Expect.equal (combineLineRight input) expected
         ]
